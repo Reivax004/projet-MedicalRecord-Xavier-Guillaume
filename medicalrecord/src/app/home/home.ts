0440ea838aa915a18b9318587fccbd0eede27eac
Qxillum <Qxillum@gmail.com>
2026-01-29 11:26:41 +0100

    Correction home

--- medicalrecord/src/app/home/home.ts
+++ medicalrecord/src/app/home/home.ts
@@ -1,17 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import {RouterLink} from '@angular/router';
-import {NgOptimizedImage} from '@angular/common';
+
 @Component({
   selector: 'app-home',
-  imports: [RouterLink, NgOptimizedImage],
+  standalone: true,
+  imports: [RouterLink],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
 export class Home implements OnInit{
-  userType: string | null = null;
+  public userType: string | null = null;
   ngOnInit(): void {
     this.userType = localStorage.getItem('userType');
     console.log('User Type:', this.userType);
   }
- 
+
 }
